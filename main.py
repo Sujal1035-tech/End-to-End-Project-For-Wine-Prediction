e532dc50952f8fab6cb166423a893bac9b4a8144
sujal1035-tech <sujalkhant4@gmail.com>
2025-03-27 05:25:17 -0700

    Data_Transformation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os
+import sys
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
 if __name__ == '__main__':
@@ -13,7 +17,7 @@ if __name__ == '__main__':
         logger.exception(e)
         raise e
 
-
+STAGE_NAME = "Data Validation stage"
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -21,5 +25,15 @@ if __name__ == '__main__':
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
         logger.exception(e)
         raise e
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,7 +1,7 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml,create_directories
-from mlProject.entity.config_entity import DataIngestionConfig,DataValidationConfig
-
+from mlProject.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig)
+#data_ingestion
 class ConfigurationManager:
     def __init__(
             self,
@@ -29,7 +29,7 @@ class ConfigurationManager:
 
         return data_ingestion_config
     
-
+    #data validation
     class ConfigurationManager:
         def __init__(
          self,
@@ -59,3 +59,30 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+
+    #data transformation
+    class ConfigurationManager:
+     def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH,
+        schema_filepath = SCHEMA_FILE_PATH):
+
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        self.schema = read_yaml(schema_filepath)
+
+        create_directories([self.config.artifacts_root])
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
